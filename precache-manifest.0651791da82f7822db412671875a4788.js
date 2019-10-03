b1a237539e8d80598f7455d628a29d2678bbf4df
paulus <paul_evu2006@yahoo.com>
2019-10-03 20:32:17 +0300

    Updates

---
+++ precache-manifest.0651791da82f7822db412671875a4788.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cab25705c922a71e23586720c3aae1de",
+    "url": "/paulus1991.io/flip/index.html"
+  },
+  {
+    "revision": "5032d36ba9d05e399148",
+    "url": "/paulus1991.io/flip/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "c9ae64a7a677424d2d0e",
+    "url": "/paulus1991.io/flip/static/css/main.0e411f4f.chunk.css"
+  },
+  {
+    "revision": "5032d36ba9d05e399148",
+    "url": "/paulus1991.io/flip/static/js/2.8fb4d6fc.chunk.js"
+  },
+  {
+    "revision": "c9ae64a7a677424d2d0e",
+    "url": "/paulus1991.io/flip/static/js/main.64d90ca5.chunk.js"
+  },
+  {
+    "revision": "acb00468516edf0f852a",
+    "url": "/paulus1991.io/flip/static/js/runtime-main.f4edcbf1.js"
+  }
+]);
